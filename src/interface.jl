0be22a656dc81d1ef8558b2f537b96ca6229dc5a
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2019-07-08 23:04:22 +0100

    now adheres to the style guide

--- src/interface.jl
+++ src/interface.jl
@@ -25,12 +25,20 @@ inverse(b::Bijector) = y -> inverse(b, y)
 # TODO: allow batch-computation, especially for univariate case
 "Computes the absolute determinant of the Jacobian of the inverse-transformation."
 logdetinvjac(b::Bijector, y) = begin end
-logdetinvjac(b::ADBijector{AD}, y::T) where {AD <: Turing.Core.ForwardDiffAD, T <: Real} = log(abs(ForwardDiff.derivative(z -> inverse(b, z), y)))
-logdetinvjac(b::ADBijector{AD}, y::AbstractVector) where AD <: Turing.Core.ForwardDiffAD = logabsdet(ForwardDiff.jacobian(z -> inverse(b, z), y))[1]
+function logdetinvjac(b::ADBijector{<: Turing.Core.ForwardDiffAD}, y::Real)
+    log(abs(ForwardDiff.derivative(z -> inverse(b, z), y)))
+end
+function logdetinvjac(b::ADBijector{<:Turing.Core.ForwardDiffAD}, y::AbstractVector{<:Real})
+    logabsdet(ForwardDiff.jacobian(z -> inverse(b, z), y))[1]
+end
 
 # FIXME: untrack? i.e. `Tracker.data(...)`
-logdetinvjac(b::ADBijector{AD}, y::T) where {AD <: Turing.Core.TrackerAD, T <: Real} = log(abs(Tracker.gradient(z -> inverse(b, z[1]), [y])[1][1]))
-logdetinvjac(b::ADBijector{AD}, y::AbstractVector) where AD <: Turing.Core.TrackerAD = logabsdet(Tracker.jacobian(z -> inverse(b, z), y))[1]
+function logdetinvjac(b::ADBijector{<: Turing.Core.TrackerAD}, y::Real)
+    log(abs(Tracker.gradient(z -> inverse(b, z[1]), [y])[1][1]))
+end
+function logdetinvjac(b::ADBijector{<: Turing.Core.TrackerAD}, y::AbstractVector{<: Real})
+    logabsdet(Tracker.jacobian(z -> inverse(b, z), y))[1]
+end
 
 # Example bijector
 struct Identity <: Bijector end
@@ -43,24 +51,28 @@ logdetinvjac(::Identity, y::AbstractVector{T}) where T <: Real = zero(T)
 struct DistributionBijector{AD, D} <: ADBijector{AD} where D <: Distribution
     dist::D
 end
-DistributionBijector(dist::D) where D <: Distribution = DistributionBijector{Turing.Core.ADBackend(), D}(dist)
+function DistributionBijector(dist::D) where D <: Distribution
+    DistributionBijector{Turing.Core.ADBackend(), D}(dist)
+end
 
 transform(b::DistributionBijector, x) = link(b.dist, x)
 inverse(b::DistributionBijector, y) = invlink(b.dist, y)
 
 # Transformed distributions
-struct UnivariateTransformed{D, B} <: Distribution{Univariate, Continuous} where {D <: UnivariateDistribution, B <: Bijector}
+struct UnivariateTransformed{D, B} <: Distribution{Univariate, Continuous}
+    where {D <: UnivariateDistribution, B <: Bijector}
     dist::D
     transform::B
 end
 
-struct MultivariateTransformed{D, B} <: Distribution{Multivariate, Continuous} where {D <: MultivariateDistribution, B <: Bijector}
+struct MultivariateTransformed{D, B} <: Distribution{Multivariate, Continuous}
+    where {D <: MultivariateDistribution, B <: Bijector}
     dist::D
     transform::B
 end
 
 
-# implement these on a case-by-case basis, e.g. `PDMatDistribution = Union{InverseWishart, Wishart}`
+# Can implement these on a case-by-case basis
 transformed(d::UnivariateDistribution, b::Bijector) = UnivariateTransformed(d, b)
 transformed(d::MultivariateDistribution, b::Bijector) = MultivariateTransformed(d, b)
 
@@ -99,7 +111,7 @@ end
 
 # rand
 rand(rng::AbstractRNG, td::UnivariateTransformed) = transform(td.transform, rand(td.dist))
-function _rand!(rng::AbstractRNG, td::MultivariateTransformed, x::AbstractVector{T} where T <: Real)
+function _rand!(rng::AbstractRNG, td::MultivariateTransformed, x::AbstractVector{<: Real})
     rand!(rng, td.dist, x)
     y = transform(td.transform, x)
     copyto!(x, y)
