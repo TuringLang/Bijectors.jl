59aeceba62cb0e66ffbfb1bce7365849a97c9aca
Sharan Yalburgi <sharanyalburgi@gmail.com>
2019-07-26 00:42:42 +0200

    fix spaces

--- src/norm_flows.jl
+++ src/norm_flows.jl
@@ -24,11 +24,11 @@ end
 
 function update_u_hat(u, w)
     # to preserve invertibility
-    u_hat = u + (planar_flow_m(transpose(w)*u) - transpose(w)*u)[1]*w/(norm(w[:,1],2)^2)
+    u_hat = u + (planar_flow_m(transpose(w) * u) - transpose(w) * u)[1] * w/(norm(w[:,1],2) ^ 2)
 end
 
 function update_u_hat!(flow::PlanarLayer)
-    flow.u_hat = flow.u + (planar_flow_m(transpose(flow.w)*flow.u) - transpose(flow.w)*flow.u)[1]*flow.w/(norm(flow.w,2)^2)
+    flow.u_hat = flow.u + (planar_flow_m(transpose(flow.w) * flow.u) - transpose(flow.w) * flow.u)[1] * flow.w/(norm(flow.w,2)^2)
 end
 
 
@@ -49,7 +49,7 @@ end
 
 planar_flow_m(x) = -1 .+ log.(1 .+ exp.(x)) # For planar flow
 dtanh(x) = 1 .- (tanh.(x)).^2 # For planar flow
-ψ(z, w, b) = dtanh(transpose(w)*z .+ b).*w # For planar flow
+ψ(z, w, b) = dtanh(transpose(w) * z .+ b) .* w # For planar flow
 h(α, r) = 1 ./ (α .+ r) # For radial flow
 dh(α, r) = -h(α, r).^2 # For radial flow
 
@@ -61,7 +61,7 @@ function transform(flow::RadialLayer, z)
     α = softplus(flow.α_[1])
     β_hat = -α + softplus(flow.β[1])
     r = norm.(z .- flow.z_not, 1)
-    return z + β_hat.*h(α, r).*(z .- flow.z_not)
+    return z + β_hat .* h(α, r) .* (z .- flow.z_not)
 end
 
 function forward(flow::T, z) where {T<:PlanarLayer}
@@ -81,6 +81,6 @@ function forward(flow::T, z) where {T<:RadialLayer}
     β_hat = -α + softplus(flow.β[1])
     r = transpose(norm.([z[:,i] .- flow.z_not[:,:] for i in 1:size(z)[2]], 1))
     d = size(flow.z_not)[1]
-    log_det_jacobian = log.(((1.0 .+ β_hat.*h(α, r)).^(d-1)) .* ( 1.0 .+  β_hat.*h(α, r) + β_hat.*dh(α, r).*r))
+    log_det_jacobian = log.(((1.0 .+ β_hat .* h(α, r)).^(d-1)) .* ( 1.0 .+  β_hat .* h(α, r) + β_hat .* dh(α, r) .* r))
     return (rv=transformed, logabsdetjacob=log_det_jacobian)
 end
