29562af15b7d71f6a6945cd2ee0ffa860e92a17d
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2019-09-13 09:58:34 +0200

    batch-specialization for Log and Exp

--- src/interface.jl
+++ src/interface.jl
@@ -325,14 +325,21 @@ struct Log{N} <: Bijector{N} end
 Exp() = Exp{0}()
 Log() = Log{0}()
 
-(b::Log)(x) = @. log(x)
 (b::Exp)(y) = @. exp(y)
+(b::Log)(x) = @. log(x)
 
-inv(b::Log{N}) where {N} = Exp{N}()
 inv(b::Exp{N}) where {N} = Log{N}()
+inv(b::Log{N}) where {N} = Exp{N}()
 
-logabsdetjac(b::Log, x) = - sum(log.(x))
-logabsdetjac(b::Exp, y) = sum(y)
+logabsdetjac(b::Exp{0}, x::Real) = x
+logabsdetjac(b::Exp{0}, x::AbstractVector) = x
+logabsdetjac(b::Exp{1}, x::AbstractVector) = sum(x)
+logabsdetjac(b::Exp{1}, x::AbstractMatrix) = vec(sum(x; dims = 1))
+
+logabsdetjac(b::Log{0}, x::Real) = -log(x)
+logabsdetjac(b::Log{0}, x::AbstractVector) = -log.(x)
+logabsdetjac(b::Log{1}, x::AbstractVector) = - sum(log.(x))
+logabsdetjac(b::Log{1}, x::AbstractMatrix) = - vec(sum(log.(x); dims = 1))
 
 #################
 # Shift & Scale #
