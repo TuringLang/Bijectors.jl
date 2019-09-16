c38f203f501abf58971182dc880d612a0bfb00b3
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2019-09-16 15:28:46 +0200

    checking something with CI

--- test/interface.jl
+++ test/interface.jl
@@ -318,6 +318,8 @@ struct NonInvertibleBijector{AD} <: ADBijector{AD} end
 
         # TODO: change when we have dimensionality in the type
         sb = vcat([Bijectors.Exp(), Bijectors.SimplexBijector()]...)
+        println(sb)
+        println(x)
         @test_throws AssertionError sb(x ./ sum(x))
 
         @testset "Stacked: ADVI with MvNormal" begin
