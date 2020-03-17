a7f43e9917615141e1db64d05919ed3b561249e9
mohamed82008 <mohamed82008@gmail.com>
2020-03-17 20:54:06 +1100

    replace_diag comment

--- src/bijectors/pd.jl
+++ src/bijectors/pd.jl
@@ -1,5 +1,8 @@
 struct PDBijector <: Bijector{2} end
 
+# This function has custom adjoints defined for Tracker, Zygote and ReverseDiff.
+# I couldn't find a mutation-free implementation that maintains TrackedArrays in Tracker
+# and ReverseDiff, hence the need for custom adjoints.
 function replace_diag(f, X)
     g(i, j) = ifelse(i == j, f(X[i, i]), X[i, j])
     return g.(1:size(X, 1), (1:size(X, 2))')
