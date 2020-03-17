0aa00599af0cbedc6e67b2d145d452901239fba6
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2020-03-17 13:16:29 +0100

    made overload of Distributions.params explicit

--- src/transformed_distribution.jl
+++ src/transformed_distribution.jl
@@ -245,7 +245,7 @@ forward(d::Distribution) = forward(GLOBAL_RNG, d)
 forward(d::Distribution, num_samples::Int) = forward(GLOBAL_RNG, d, num_samples)
 
 # utility stuff
-params(td::Transformed) = params(td.dist)
+Distributions.params(td::Transformed) = Distributions.params(td.dist)
 function Base.maximum(td::UnivariateTransformed)
     # ordering might have changed, i.e. ub has been mapped to lb
     min, max = td.transform.((Base.minimum(td.dist), Base.maximum(td.dist)))
