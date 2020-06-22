446968d4d1beefce36c25f69bf5ac0fbc56ee192
David Widmann <devmotion@users.noreply.github.com>
2020-06-22 12:17:02 +0200

    Remove use of `eval` in tests (#116)
    * Remove use of `eval` in tests
    
    * Simplify handling of `Diagonal` matrices
    
    * Fix version of Adapt for now (bugfix for Tracker 0.2.7)
    
    * Remove comments

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,32 @@
-using ReverseDiff, Tracker, ForwardDiff, Zygote, DistributionsAD, Bijectors
-using Random, LinearAlgebra, Combinatorics, Test
-using DistributionsAD: TuringUniform, TuringMvNormal, TuringMvLogNormal, 
-                        TuringPoissonBinomial
+using Bijectors
 
-Random.seed!(123456)
+using Combinatorics
+using DistributionsAD
+using FiniteDiff
+using ForwardDiff
+using ReverseDiff
+using Tracker
+using Zygote
 
-function get_stage()
-    if get(ENV, "TRAVIS", "") == "true" || get(ENV, "GITHUB_ACTIONS", "") == "true"
-        if "STAGE" in keys(ENV)
-            return ENV["STAGE"]
-        else
-            return "nonAD"
-        end
-    else
-        if "STAGE" in keys(ENV)
-            return ENV["STAGE"]
-        else
-            return "all"
-        end
-    end
-end
+using Random, LinearAlgebra, Test
 
-stg = get_stage()
-if stg in ("nonAD", "all")
-    @testset "Interface" begin
-        include("interface.jl")
-    end
+using Bijectors: Log, Exp, Shift, Scale, Logit, SimplexBijector, PDBijector, Permute,
+    PlanarLayer, RadialLayer, Stacked, TruncatedBijector, ADBijector
+
+using DistributionsAD: TuringUniform, TuringMvNormal, TuringMvLogNormal,
+    TuringPoissonBinomial
+
+const is_TRAVIS = haskey(ENV, "TRAVIS")
+const GROUP = get(ENV, "GROUP", "All")
+
+if GROUP == "All" || GROUP == "Interface"
+    include("interface.jl")
     include("transform.jl")
     include("norm_flows.jl")
     include("bijectors/permute.jl")
 end
-if stg != "nonAD"
-    include("ad/ad_test_utils.jl")
+
+if !is_TRAVIS && (GROUP == "All" || GROUP == "AD")
+    include("ad/utils.jl")
     include("ad/distributions.jl")
 end
