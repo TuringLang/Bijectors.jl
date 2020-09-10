fd2e33be2a931077ea5988ed9c2935ead893d3d7
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-09-10 07:48:51 +0200

    using masks rather than ifelse for type-stability and simplicity

--- src/bijectors/leaky_relu.jl
+++ src/bijectors/leaky_relu.jl
@@ -11,28 +11,28 @@ struct LeakyReLU{T, N} <: Bijector{N}
     α::T
 end
 
-
 LeakyReLU(α::T; dim::Val{N} = Val(0)) where {T<:Real, N} = LeakyReLU{T, N}(α)
 LeakyReLU(α::T; dim::Val{N} = Val(D)) where {D, T<:AbstractArray{<:Real, D}, N} = LeakyReLU{T, N}(α)
 
+up1(b::LeakyReLU{T, N}) where {T, N} = LeakyReLU{T, N + 1}(b.α)
+
 # (N=0) Univariate case
-function (b::LeakyReLU{T1, 0})(x::T2) where {T1<:Real, T2<:Real}
-    T = promote_type(T1, T2)
-    return T(ifelse(x < zero(T2), b.α * x, x))
+function (b::LeakyReLU{<:Any, 0})(x::Real)
+    mask = x < zero(x)
+    return mask * b.α * x + (1 - mask) * x
 end
-(b::LeakyReLU{<:Real, 0})(x::AbstractVector{<:Real}) = b.(x)
+(b::LeakyReLU{<:Any, 0})(x::AbstractVector{<:Real}) = b.(x)
 
-function (ib::Inversed{<:LeakyReLU{T1}, 0})(y::T2) where {T1<:Real, T2<:Real}
-    T = promote_type(T1, T2)
-    return T(ifelse(y < zero(T2), inv(ib.orig.α) * y, y))
+function (ib::Inverse{<:LeakyReLU, 0})(y::Real)
+    mask = y < zero(y)
+    return mask * (x / b.α) + (1 - mask) * x
 end
-(ib::Inversed{<:LeakyReLU{<:Real}, 0})(y::AbstractVector{<:Real}) = ib.(y)
+(ib::Inverse{<:LeakyReLU{<:Any}, 0})(y::AbstractVector{<:Real}) = ib.(y)
 
-function logabsdetjac(b::LeakyReLU{T1, 0}, x::T2) where {T1<:Real, T2<:Real}
-    T = promote_type(T1, T2)
-    J⁻¹ = T(ifelse(x < 0, b.α, one(T2)))
-
-    return log.(abs.(J⁻¹))
+function logabsdetjac(b::LeakyReLU{<:Any, 0}, x::Real)
+    mask = x < zero(x)
+    J = mask * b.α + (1 - mask) * one(x)
+    return log.(abs.(J))
 end
 logabsdetjac(b::LeakyReLU{<:Real, 0}, x::AbstractVector{<:Real}) = logabsdetjac.(b, x)
 
@@ -40,56 +40,49 @@ logabsdetjac(b::LeakyReLU{<:Real, 0}, x::AbstractVector{<:Real}) = logabsdetjac.
 # We implement `forward` by hand since we can re-use the computation of
 # the Jacobian of the transformation. This will lead to faster sampling
 # when using `rand` on a `TransformedDistribution` making use of `LeakyReLU`.
-function forward(b::LeakyReLU{T1, 0}, x::T2) where {T1<:Real, T2<:Real}
-    T = promote_type(T1, T2)
-    J = T(ifelse(x < 0, b.α, one(T2))) # <= is really diagonal of jacobian
+function forward(b::LeakyReLU{<:Any, 0}, x::Real)
+    mask = x < zero(x)
+    J = mask * b.α + (1 - mask) * one(x)
     return (rv=J * x, logabsdetjac=log(abs(J)))
 end
 
 # Batched version
-function forward(b::LeakyReLU{T1, 0}, x::AbstractVector{T2}) where {T1<:Real, T2<:Real}
-    T = promote_type(T1, T2)
-    J = @. T(ifelse(x < 0, b.α, one(T2))) # <= is really diagonal of jacobian
+function forward(b::LeakyReLU{<:Any, 0}, x::AbstractVector)
+    mask = x .< zero(eltype(x))
+    J = mask .* b.α .+ (1 .- mask) .* one(eltype(x))
     return (rv=J .* x, logabsdetjac=log.(abs.(J)))
 end
 
 # (N=1) Multivariate case, with univariate parameter `α`
-function (b::LeakyReLU{T1, 1})(x::AbstractVecOrMat{T2}) where {T1<:Real, T2}
-    # Note that this will do the correct thing even for `Tracker.jl` in the sense
-    # that the resulting array will be `TrackedArray` rather than `Array{<:TrackedReal}`.
-    T = promote_type(T1, T2)
-    return @. T(ifelse(x < zero(T2), b.α * x, x))
+function (b::LeakyReLU{<:Any, 1})(x::AbstractVecOrMat)
+    mask = x .< zero(eltype(x))
+    return mask .* b.α .* x .+ (1 .- mask) .* x
 end
 
-function (ib::Inversed{<:LeakyReLU{T1}, 1})(y::AbstractVecOrMat{T2}) where {T1<:Real, T2<:Real}
-    # Note that this will do the correct thing even for `Tracker.jl` in the sense
-    # that the resulting array will be `TrackedArray` rather than `Array{<:TrackedReal}`.
-    T = promote_type(T1, T2)
-    return @. T(ifelse(y < zero(T2), inv(ib.orig.α) * y, y))
+function (ib::Inverse{<:LeakyReLU, 1})(y::AbstractVecOrMat)
+    mask = x .< zero(eltype(y))
+    return mask .* (y ./ ib.orig.α) .+ (1 .- mask) .* y
 end
 
-function logabsdetjac(b::LeakyReLU{T1, 1}, x::AbstractVecOrMat{T2}) where {T1<:Real, T2<:Real}
-    T = promote_type(T1, T2)
-
+function logabsdetjac(b::LeakyReLU{<:Any, 1}, x::AbstractVecOrMat)
     # Is really diagonal of jacobian
-    J⁻¹ = @. T(ifelse(x < 0, b.α, one(T2)))
+    mask = x .< zero(eltype(x))
+    J = mask .* b.α .+ (1 .- mask) .* one(eltype(x))
 
     if x isa AbstractVector
-        return sum(log.(abs.(J⁻¹)))
+        return sum(log.(abs.(J)))
     elseif x isa AbstractMatrix
-        return vec(sum(log.(abs.(J⁻¹)); dims = 1))  # sum along column
+        return vec(sum(log.(abs.(J)); dims = 1))  # sum along column
     end
 end
 
 # We implement `forward` by hand since we can re-use the computation of
 # the Jacobian of the transformation. This will lead to faster sampling
 # when using `rand` on a `TransformedDistribution` making use of `LeakyReLU`.
-function forward(b::LeakyReLU{T1, 1}, x::AbstractVecOrMat{T2}) where {T1<:Real, T2<:Real}
-    # Note that this will do the correct thing even for `Tracker.jl` in the sense
-    # that the resulting array will be `TrackedArray` rather than `Array{<:TrackedReal}`.
-    T = promote_type(T1, T2)
-
-    J = @. T(ifelse(x < 0, b.α, one(T2))) # <= is really diagonal of jacobian
+function forward(b::LeakyReLU{<:Any, 1}, x::AbstractVecOrMat)
+    # Is really diagonal of jacobian
+    mask = x .< zero(eltype(x))
+    J = mask .* b.α .+ (1 .- mask) .* one(eltype(x))
 
     if x isa AbstractVector
         logjac = sum(log.(abs.(J)))
