70718df1427db7f37c8939d508999d73a166610d
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-09-11 15:16:58 +0200

    added proper testing to RQS and utils for Bijectors

--- test/bijectors/rational_quadratic_spline.jl
+++ test/bijectors/rational_quadratic_spline.jl
@@ -2,9 +2,9 @@ using Test
 using Bijectors
 using Bijectors: RationalQuadraticSpline
 
-@setset "RationalQuadraticSpline" begin
+@testset "RationalQuadraticSpline" begin
     # Monotonic spline on '[-B, B]' with `K` intermediate knots/"connection points".
-    K = 3; B = 2;
+    d = 2; K = 3; B = 2;
     b_uv = RationalQuadraticSpline(randn(K), randn(K), randn(K - 1), B)
     b_mv = RationalQuadraticSpline(randn(d, K), randn(d, K), randn(d, K - 1), B)
 
@@ -38,71 +38,23 @@ using Bijectors: RationalQuadraticSpline
 
         # Inside of domain
         x = 0.5
-        @test b(x) ≠ x
-        @test logabsdetjac(b, x) ≠ 0
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
-        x = [-x, x] # batch
-        @test b(x) ≠ x
-        @test b(x) isa Vector
-        @test logabsdetjac(b, x) ≠ zeros(length(x))
-        @test logabsdetjac(b, x) isa Vector
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
+        test_bijector(b, [-x, x])
 
         # Outside of domain
         x = 5.
-        @test b(x) == x
-        @test logabsdetjac(b, x) == 0
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
-        x = [-x, x] # batch
-        @test b(x) == x
-        @test b(x) isa Vector
-        @test logabsdetjac(b, x) == zeros(length(x))
-        @test logabsdetjac(b, x) isa Vector
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
+        test_bijector(b, [-x, x], [-x, x], [0., 0.])
 
         # multivariate
         b = b_mv
 
         # Inside of domain
         x = [-0.5, 0.5]
-        @test b(x) ≠ x
-        @test b(x) isa Vector
-        @test logabsdetjac(b, x) ≠ 0
-        @test logabsdetjac(b, x) isa Real
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
-        x = hcat(-x, x) # batch
-        @test b(x) ≠ x
-        @test b(x) isa Matrix
-        @test logabsdetjac(b, x) ≠ zeros(size(x, 2))
-        @test logabsdetjac(b, x) isa Vector
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
+        x = hcat(x, -x, x) # batch
+        test_bijector(b, x)
 
         # Outside of domain
         x = [-5., 5.]
-        @test b(x) == x
-        @test logabsdetjac(b, x) == 0
-        @test logabsdetjac(b, x) isa Real
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
-        x = hcat(-x, x) # batch
-        @test b(x) == x
-        @test logabsdetjac(b, x) == zeros(size(x, 2))
-        @test logabsdetjac(b, x) isa Vector
-        res = forward(b, x)
-        @test res.rv ≈ b(x)
-        @test res.logabsdetjac ≈ logabsdetjac(b, x)
+        x = hcat(x, -x, x) # batch
+        test_bijector(b, x, x, zeros(size(x, 2)))
     end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -19,6 +19,10 @@ using DistributionsAD: TuringUniform, TuringMvNormal, TuringMvLogNormal,
 const is_TRAVIS = haskey(ENV, "TRAVIS")
 const GROUP = get(ENV, "GROUP", "All")
 
+# Always include this since it can be useful for other tests.
+include("ad/utils.jl")
+include("bijectors/utils.jl")
+
 if GROUP == "All" || GROUP == "Interface"
     include("interface.jl")
     include("transform.jl")
@@ -28,6 +32,5 @@ if GROUP == "All" || GROUP == "Interface"
 end
 
 if !is_TRAVIS && (GROUP == "All" || GROUP == "AD")
-    include("ad/utils.jl")
     include("ad/distributions.jl")
 end
