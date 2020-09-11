b622331747e114726c5a9e104f276183f2de11d2
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-09-11 11:00:15 +0200

    cleaned up RQS significantly

--- src/bijectors/rational_quadratic_spline.jl
+++ src/bijectors/rational_quadratic_spline.jl
@@ -1,82 +1,60 @@
 using NNlib
+using Bijectors
 
-abstract type Constrained end
-
-"""
-    value(c::Constrained)
-
-Return the value which is wrapped.
-"""
-value(c::Constrained)
-
-struct Unconstrained{T, F} <: Constrained
-    val::T
-end
-Unconstrained(val::T, f) where {T} = Unconstrained{T, f}(val)
-Unconstrained(val::T) where {T} = Unconstrained{T, identity}(val)
-
-value(c::Unconstrained{T, F}) where {T, F} = F(c.val)
-f(::Unconstrained{T, F}) where {T, F} = F
-
-
-struct RationalQuadraticSpline{T, D} <: Bijector{D}
+struct RationalQuadraticSpline{T, N} <: Bijector{N}
     widths::T      # K widths
     heights::T     # K heights
-    derivatives::T # (K - 1) derivatives
+    derivatives::T # K derivatives, with endpoints being ones
 
     function RationalQuadraticSpline(widths::T, heights::T, derivatives::T) where {T<:AbstractVector}
-        # FIXME: add a `NoArgCheck` type and argument so we can circumvent if we want
-        
+        # TODO: add a `NoArgCheck` type and argument so we can circumvent if we want        
         @assert length(widths) == length(heights) == length(derivatives)
-        # @assert all(widths .> 0) "widths need to be positive"
-        # @assert all(heights .> 0) "heights need to be positive"
-        # @assert widths[1] ≈ heights[1] "widths and heights need equal left endpoint"
-        # @assert widths[end] ≈ heights[end] "widths and heights need equal right endpoint"
-        # @assert sum(widths) ≈ sum(heights) "widths and heights should sum to 2B"  # should both sum to 2B s.t. [-B, B] × [-B, B]
         @assert all(derivatives .> 0) "derivatives need to be positive"
         
         return new{T, 0}(widths, heights, derivatives)
     end
 
     function RationalQuadraticSpline(widths::T, heights::T, derivatives::T) where {T<:AbstractMatrix}
+        @assert size(widths, 2) == size(heights, 2) == size(derivatives, 2)
+        @assert all(derivatives .> 0) "derivatives need to be positive"
         return new{T, 1}(widths, heights, derivatives)
     end
 end
 
 function RationalQuadraticSpline(
-    widths::Unconstrained{A, identity},
-    heights::Unconstrained{A, identity},
-    derivatives::Unconstrained{A, identity},
+    widths::A,
+    heights::A, 
+    derivatives::A,
     B::T2
 ) where {T1, T2, A <: AbstractVector{T1}}
     # Using `NNLlinb.softax` instead of `StatsFuns.softmax` (which does inplace operations)
     return RationalQuadraticSpline(
-        (cumsum(vcat([zero(T1)], NNlib.softmax(value(widths)))) .- 0.5) * 2 * B,
-        (cumsum(vcat([zero(T1)], NNlib.softmax(value(heights)))) .- 0.5) * 2 * B,
-        vcat([one(T1)], softplus.(value(derivatives)), [one(T1)])
+        (cumsum(vcat([zero(T1)], NNlib.softmax(widths))) .- 0.5) * 2 * B,
+        (cumsum(vcat([zero(T1)], NNlib.softmax(heights))) .- 0.5) * 2 * B,
+        vcat([one(T1)], softplus.(derivatives), [one(T1)])
     )
 end
 
 # TODO: implement transformations for the case where `Dim == 1`
 function RationalQuadraticSpline(
-    widths::Unconstrained{A, identity},
-    heights::Unconstrained{A, identity},
-    derivatives::Unconstrained{A, identity},
+    widths::A,
+    heights::A,
+    derivatives::A,
     B::T2
 ) where {T1, T2, A <: AbstractMatrix{T1}}
-    # TODO: use `NNlib.softmax(widths; dims = 2)` when new version of `NNlib` is released
-    
-    # Using `NNLlinb.softax` instead of `StatsFuns.softmax` (which does inplace operations)
+    ws = hcat(zeros(T1, size(widths, 1)), NNlib.softmax(widths; dims = 2))
+    hs = hcat(zeros(T1, size(widths, 1)), NNlib.softmax(heights; dims = 2))
+    ds = hcat(ones(T1, size(widths, 1)), softplus.(derivatives), ones(T1, size(widths, 1)))
+
     return RationalQuadraticSpline(
-        hcat([(cumsum(vcat([zero(T1)], NNlib.softmax(value(widths)[:, i]))) .- 0.5) * 2 * B for i = 1:size(value(widths), 2)]...),
-        hcat([(cumsum(vcat([zero(T1)], NNlib.softmax(value(heights)[:, i]))) .- 0.5) * 2 * B for i = 1:size(value(heights), 2)]...),
-        hcat([vcat([one(T1)], softplus.(value(derivatives)[:, i]), [one(T1)]) for i = 1:size(value(derivatives), 2)]...)
+        (2 * B) .* (cumsum(ws; dims = 2) .- 0.5),
+        (2 * B) .* (cumsum(hs; dims = 2) .- 0.5),
+        ds
     )
 end
 
 function rqs_univariate(widths, heights, derivatives, x::Real)
     K = length(widths) - 1
-    # @assert K == length(x) "length(x) should be the same length as the widhts/heights"
 
     # Find which bin `x` is in; subtract 1 because `searchsortedfirst` returns idx of ≥ not ≤
     k = searchsortedfirst(widths, x) - 1
@@ -145,22 +123,21 @@ function rqs_univariate_inverse(widths, heights, derivatives, y::Real)
     numerator = - 2 * a3
     denominator = (a2 + sqrt(a2^2 - 4 * a1 * a3))
     ξ = numerator / denominator
-
-    # @assert isapprox(a1 * ξ^2 + a2 * ξ + a3, 0.0, atol=1e-8)
     
     return ξ * w + widths[k]
 end
-(ib::Inversed{<:RationalQuadraticSpline, 0})(y::Real) = rqs_univariate_inverse(ib.orig.widths, ib.orig.heights, ib.orig.derivatives, y)
-(ib::Inversed{<:RationalQuadraticSpline, 0})(y::AbstractVector) = ib.(y)
+(ib::Inverse{<:RationalQuadraticSpline, 0})(y::Real) = rqs_univariate_inverse(ib.orig.widths, ib.orig.heights, ib.orig.derivatives, y)
+(ib::Inverse{<:RationalQuadraticSpline, 0})(y::AbstractVector) = ib.(y)
 
-function (ib::Inversed{<:RationalQuadraticSpline, 1})(y::AbstractVector)
+function (ib::Inverse{<:RationalQuadraticSpline, 1})(y::AbstractVector)
     b = ib.orig
     @assert length(y) == size(b.widths, 2) == size(b.heights, 2) == size(b.derivatives, 2)
-    
+
     return [rqs_univariate_inverse(b.widths[:, i], b.heights[:, i], b.derivatives[:, i], y[i]) for i = 1:length(y)]
 end
-function (ib::Inversed{<:RationalQuadraticSpline, 1})(y::AbstractMatrix)
-    return foldl(hcat, [ib(y[:, i]) for i = 1:size(y, 2)])
+function (ib::Inverse{<:RationalQuadraticSpline, 1})(y::AbstractMatrix)
+    return mapreduce(ib, hcat, eachcol(y))
+    # return foldl(hcat, [ib(y[:, i]) for i = 1:size(y, 2)])
 end
 
 function rqs_logabsdetjac(widths, heights, derivatives, x::Real)
@@ -194,7 +171,7 @@ function logabsdetjac(b::RationalQuadraticSpline{<:AbstractMatrix, 1}, x::Abstra
     return sum([rqs_logabsdetjac(b.widths[:, i], b.heights[:, i], b.derivatives[:, i], x[i]) for i = 1:length(x)])
 end
 function logabsdetjac(b::RationalQuadraticSpline{<:AbstractMatrix, 1}, x::AbstractMatrix)
-    return [logabsdetjac(b, x[:, i]) for i = 1:size(x, 2)]
+    return map(logabsdetjac, eachcol(x))
 end
 
 # TODO: implement this for `x::AbstractVector` and similarily for 1-dimensional `b`
