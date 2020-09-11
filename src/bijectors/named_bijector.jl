79ed015325df9077d62e3f96ff1f98e92dfaedd8
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-09-11 08:15:21 +0200

    added NamedComposition and some tests

--- src/bijectors/named_bijector.jl
+++ src/bijectors/named_bijector.jl
@@ -1,45 +1,32 @@
 abstract type AbstractNamedBijector <: AbstractBijector end
 
+forward(b::AbstractBijector, x) = (rv = b(x), logabsdetjac = logabsdetjac(b, x))
+
+#######################
+### `NamedBijector` ###
+#######################
 struct NamedBijector{names, Bs<:NamedTuple{names}} <: AbstractNamedBijector
     bs::Bs
 end
 
 @inline names_to_bijectors(b::NamedBijector) = b.bs
 
-@generated function (b::NamedBijector{names})(x::NamedTuple{names}) where {names}
-    return :(($([:($n = b.bs.$n(x.$n)) for n in names]...), ))
+@generated function (b::NamedBijector{names})(x::NamedTuple) where {names}
+    return :(merge(x, ($([:($n = b.bs.$n(x.$n)) for n in names]...), )))
 end
 
 @generated function Base.inv(b::NamedBijector{names}) where {names}
     return :(NamedBijector(($([:($n = inv(b.bs.$n)) for n in names]...), )))
 end
 
-@generated function logabsdetjac(b::NamedBijector{names}, x::NamedTuple{names}) where {names}
+@generated function logabsdetjac(b::NamedBijector{names}, x::NamedTuple) where {names}
     return :(sum([$([:(logabsdetjac(b.bs.$n, x.$n)) for n in names]...), ]))
 end
 
-# FIXME: figure out how to do compositions between, e.g. `NamedBijector` and `NamedCoupling`
-# Coouuld just do the same as we've done for `Bijector`, but it's a bit annoying and seems like a bit too much?
-@generated function ∘(
-        b1::NamedBijector{names}, 
-        b2::NamedBijector{names}
-) where {names}
-    return :(NamedBijector(($([:($n = b1.bs.$n ∘ b2.bs.$n) for n in names]...), )))
-end
 
-# TODO: figure out a good way to do `composel` using generated functions?
-function composel(bs::NamedBijector{names}...) where {names}
-    return NamedBijector((; ((n, composel([b.bs[n] for b in bs]...)) for n in names)...))
-end
-
-function composer(bs::NamedBijector{names}...) where {names}
-    return NamedBijector((; ((n, composer([b.bs[n] for b in bs]...)) for n in names)...))
-end
-
-
-####################
-### NamedInverse ###
-####################
+######################
+### `NamedInverse` ###
+######################
 struct NamedInverse{B<:AbstractNamedBijector} <: AbstractNamedBijector
     orig::B
 end
@@ -47,6 +34,97 @@ Base.inv(nb::AbstractNamedBijector) = NamedInverse(nb)
 Base.inv(ni::NamedInverse) = ni.orig
 
 logabsdetjac(ni::NamedInverse, y::NamedTuple) = -logabsdetjac(inv(ni), ni(y))
+
+##########################
+### `NamedComposition` ###
+##########################
+struct NamedComposition{Bs} <: AbstractNamedBijector
+    bs::Bs
+end
+
+# Essentially just copy-paste from impl of composition for 'standard' bijectors,
+# with minor changes here and there.
+composel(bs::AbstractNamedBijector...) = NamedComposition(bs)
+composer(bs::AbstractNamedBijector...) = NamedComposition(reverse(bs))
+∘(b1::AbstractNamedBijector, b2::AbstractNamedBijector) = composel(b2, b1)
+
+function (cb::NamedComposition{<:AbstractArray{<:AbstractNamedBijector}})(x)
+    @assert length(cb.bs) > 0
+    res = cb.bs[1](x)
+    for b ∈ Base.Iterators.drop(cb.bs, 1)
+        res = b(res)
+    end
+
+    return res
+end
+
+@generated function (cb::NamedComposition{T})(x) where {T<:Tuple}
+    @assert length(T.parameters) > 0
+    expr = :(x)
+    for i in 1:length(T.parameters)
+        expr = :(cb.bs[$i]($expr))
+    end
+    return expr
+end
+
+function logabsdetjac(cb::NamedComposition, x)
+    y, logjac = forward(cb.bs[1], x)
+    for i = 2:length(cb.bs)
+        res = forward(cb.bs[i], y)
+        y = res.rv
+        logjac += res.logabsdetjac
+    end
+
+    return logjac
+end
+
+@generated function logabsdetjac(cb::NamedComposition{T}, x) where {T<:Tuple}
+    N = length(T.parameters)
+
+    expr = Expr(:block)
+    push!(expr.args, :((y, logjac) = forward(cb.bs[1], x)))
+
+    for i = 2:N - 1
+        temp = gensym(:res)
+        push!(expr.args, :($temp = forward(cb.bs[$i], y)))
+        push!(expr.args, :(y = $temp.rv))
+        push!(expr.args, :(logjac += $temp.logabsdetjac))
+    end
+    # don't need to evaluate the last bijector, only it's `logabsdetjac`
+    push!(expr.args, :(logjac += logabsdetjac(cb.bs[$N], y)))
+
+    push!(expr.args, :(return logjac))
+
+    return expr
+end
+
+
+function forward(cb::NamedComposition, x)
+    rv, logjac = forward(cb.bs[1], x)
+    
+    for t in cb.bs[2:end]
+        res = forward(t, rv)
+        rv = res.rv
+        logjac = res.logabsdetjac + logjac
+    end
+    return (rv=rv, logabsdetjac=logjac)
+end
+
+
+@generated function forward(cb::NamedComposition{T}, x) where {T<:Tuple}
+    expr = Expr(:block)
+    push!(expr.args, :((y, logjac) = forward(cb.bs[1], x)))
+    for i = 2:length(T.parameters)
+        temp = gensym(:temp)
+        push!(expr.args, :($temp = forward(cb.bs[$i], y)))
+        push!(expr.args, :(y = $temp.rv))
+        push!(expr.args, :(logjac += $temp.logabsdetjac))
+    end
+    push!(expr.args, :(return (rv = y, logabsdetjac = logjac)))
+
+    return expr
+end
+
 
 ############################
 ### `NamedCouplingLayer` ###
--- test/bijectors/named_bijector.jl
+++ test/bijectors/named_bijector.jl
@@ -1,10 +1,22 @@
 using Test
 using Bijectors
-using Bijectors: Exp, Log, Logit, AbstractNamedBijector, NamedBijector, NamedInverse, NamedCoupling
+using Bijectors: Exp, Log, Logit, AbstractNamedBijector, NamedBijector, NamedInverse, NamedCoupling, NamedComposition, Shift
 
 @testset "NamedBijector" begin
     b = NamedBijector((a = Exp(), b = Log()))
     @test b((a = 0.0, b = exp(1.0))) == (a = 1.0, b = 1.0)
+end
+
+@testset "NamedComposition" begin
+    b = NamedBijector((a = Exp(), ))
+    x = (a = 0., b = 1.)
+
+    nc1 = NamedComposition((b, b))
+    @test nc1(x) == b(b(x))
+    @test logabsdetjac(nc1, x) ≈ logabsdetjac(b, x) + logabsdetjac(b, b(x))
+
+    nc2 = b ∘ b
+    @test nc1 == nc2
 end
 
 @testset "NamedCoupling" begin
@@ -29,5 +41,10 @@ end
     x = (a = 0.0, b = 2.0, c = 1.0)
     nc = NamedCoupling(:c, (:a, :b), (a, b) -> Logit(a, b))
     @test nc(x).c == 0.0
+    @test inv(nc)(nc(x)) == x
+
+    x = (a = 0.0, b = 2.0, c = 1.0)
+    nc = NamedCoupling(:c, (:b, ), b -> Shift(b))
+    @test nc(x).c == 3.0
     @test inv(nc)(nc(x)) == x
 end
