ad367f3b67892f89e6b21d42a9b2783c23a0ca55
David Widmann <devmotion@users.noreply.github.com>
2021-01-08 03:30:39 +0100

    Smarter choice of initial bracket

--- src/bijectors/planar_layer.jl
+++ src/bijectors/planar_layer.jl
@@ -96,7 +96,7 @@ end
 """
     find_alpha(wt_y, wt_u_hat, b)
 
-Compute an (approximate) real-valued solution ``α`` to the equation
+Compute an (approximate) real-valued solution ``α̂`` to the equation
 ```math
 wt_y = α + wt_u_hat tanh(α + b)
 ```
@@ -104,22 +104,30 @@ wt_y = α + wt_u_hat tanh(α + b)
 The uniqueness of the solution is guaranteed since ``wt_u_hat ≥ -1``.
 For details see appendix A.1 of the reference.
 
+# Initial bracket
+
+For all ``α``, we have
+```math
+α - |wt_u_hat| - wt_y \\leq α + wt_u_hat tanh(α + b) - wt_y \\leq α + |wt_u_hat| - wt_y.
+```
+Thus
+```math
+α̂ - |wt_u_hat| - wt_y \\leq 0 \\leq α̂ + |wt_u_hat| - wt_y,
+```
+which implies ``α̂ ∈ [wt_y - |wt_u_hat|, wt_y + |wt_u_hat|]``.
+
 # References
 
 D. Rezende, S. Mohamed (2015): Variational Inference with Normalizing Flows.
 arXiv:1505.05770
 """
 function find_alpha(wt_y, wt_u_hat, b)
-    # Compute the initial bracket ((-Inf, 0) or (0, Inf))
-    f0 = wt_u_hat * tanh(b) - wt_y
-    zero_f0 = zero(f0)
-    if f0 < zero_f0
-        initial_bracket = (zero_f0, oftype(f0, Inf))
-    else
-        initial_bracket = (oftype(f0, -Inf), zero_f0)
-    end
-    prob = NonlinearSolve.NonlinearProblem{false}(initial_bracket) do x, _
-        x + wt_u_hat * tanh(x + b) - wt_y
+    # Compute the initial bracket.
+    _wt_y, _wt_u_hat, _b = promote(wt_y, wt_u_hat, b)
+    initial_bracket = (_wt_y - abs(_wt_u_hat), _wt_y + abs(_wt_u_hat))
+
+    prob = NonlinearSolve.NonlinearProblem{false}(initial_bracket) do α, _
+        α + _wt_u_hat * tanh(α + _b) - _wt_y
     end
     alpha = NonlinearSolve.solve(prob, NonlinearSolve.Falsi()).left
     return alpha
