6e293afb22ede92d3445bb082cea7f7ca7955675
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-06-15 19:43:49 +0100

    added tests using ChainRulesTestUtils

--- test/bijectors/ordered.jl
+++ test/bijectors/ordered.jl
@@ -10,12 +10,19 @@ import Bijectors: OrderedBijector
 
     # Larger
     x = randn(5)
-    test_bijector(b, hcat(x, x))
+    xs = hcat(x, x)
+    test_bijector(b, xs)
 
     y = b(x)
     @test sort(y) == y
 
-    ys = b(hcat(x, x))
+    ys = b(xs)
     @test sort(ys[:, 1]) == ys[:, 1]
     @test sort(ys[:, 2]) == ys[:, 2]
+
+    # `ChainRules`
+    test_rrule(Bijectors._transform_ordered, x)
+    test_rrule(Bijectors._transform_ordered, xs)
+    test_rrule(Bijectors._transform_inverse_ordered, y)
+    test_rrule(Bijectors._transform_inverse_ordered, ys)
 end
