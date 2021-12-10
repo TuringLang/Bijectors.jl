f6385ea4f2d2af8fe4fadfe06c4584411e95767b
Oliver Schulz <oschulz@mpp.mpg.de>
2021-12-11 00:19:51 +0100

    Improve deprecation scheme for forward
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/Bijectors.jl
+++ src/Bijectors.jl
@@ -252,7 +252,7 @@ include("utils.jl")
 include("interface.jl")
 include("chainrules.jl")
 
-Base.@deprecate forward(b::AbstractBijector, x) with_logabsdet_jacobian(b, x)
+Base.@deprecate forward(b::AbstractBijector, x) NamedTuple{(:rv,:logabsdetjac)}(with_logabsdet_jacobian(b, x))
 
 import Base.inv
 Base.@deprecate inv(b::AbstractBijector) inverse(b)
