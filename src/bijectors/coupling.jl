34f7fc697259f7d967bc70bc1a0c5c2e082d5b81
Oliver Schulz <oschulz@mpp.mpg.de>
2021-12-12 15:49:36 +0100

    Add rrules for combine with PartitionMask
    Zygote-generated pullback for `combine(m::PartitionMask, x_1, x_2, x_3)`
    fails with `no method matching zero(::Type{Nothing})`.

--- src/bijectors/coupling.jl
+++ src/bijectors/coupling.jl
@@ -118,6 +118,20 @@ Combines `x_1`, `x_2`, and `x_3` into a single vector.
 """
 @inline combine(m::PartitionMask, x_1, x_2, x_3) = m.A_1 * x_1 .+ m.A_2 * x_2 .+ m.A_3 * x_3
 
+function ChainRulesCore.rrule(::typeof(combine), m::PartitionMask, x_1, x_2, x_3)
+    prj = map(ChainRulesCore.ProjectTo, (x_1, x_2, x_3))
+
+    function _transform_ordered_adjoint(ΔΩ)
+        Δ = ChainRulesCore.unthunk(ΔΩ)
+        dx_1, dx_2, dx_3 = partition(m, Δ)
+
+        return ChainRulesCore.NoTangent(), ChainRulesCore.NoTangent(), prj[1](dx_1), prj[2](dx_2), prj[3](dx_3)
+    end
+
+    return combine(m, x_1, x_2, x_3), _transform_ordered_adjoint
+end
+
+
 """
     partition(m::PartitionMask, x)
 
@@ -195,6 +209,7 @@ function couple(cl::Coupling, x::AbstractVector)
     return b
 end
 
+#!!!!!!!!!!!!!
 function (cl::Coupling)(x::AbstractVector)
     # partition vector using `cl.mask::PartitionMask`
     x_1, x_2, x_3 = partition(cl.mask, x)
