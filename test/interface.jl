6ed6fa9bba457cb4b876c435f817497144b69ddf
Oliver Schulz <oschulz@mpp.mpg.de>
2021-12-12 22:57:41 +0100

    Use test_inverse and test_with_logabsdet_jacobian

--- test/interface.jl
+++ test/interface.jl
@@ -854,6 +854,15 @@ end
     end
 end
 
+@testset "test_inverse and test_with_logabsdet_jacobian" begin
+    b = Bijectors.Scale{Float64,0}(4.2)
+    x = 0.3
+
+    InverseFunctions.test_inverse(b, x)
+    ChangesOfVariables.test_with_logabsdet_jacobian(b, x, (f::Bijectors.Scale, x) -> f.a)
+end
+
+
 @testset "deprecations" begin
     b = Bijectors.Exp()
     x = 0.3
