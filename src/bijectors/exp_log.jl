fa469b8bec97b5f206fb9d3d1e28b5e84b639f8b
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2022-02-11 11:27:30 +0000

    removed unnecessary impls of with_logabsdet_jacobian

--- src/bijectors/exp_log.jl
+++ src/bijectors/exp_log.jl
@@ -15,21 +15,3 @@ logabsdetjac(b::Elementwise{typeof(exp)}, x) = sum(x)
 
 logabsdetjac(b::typeof(log), x::Real) = -log(x)
 logabsdetjac(b::Elementwise{typeof(log)}, x) = -sum(log, x)
-
-function with_logabsdet_jacobian(b::typeof(exp), x::Real)
-    y = b(x)
-    return y, x
-end
-function with_logabsdet_jacobian(b::Elementwise{typeof(exp)}, x)
-    y = b(x)
-    return y, sum(x)
-end
-
-function with_logabsdet_jacobian(b::typeof(log), y::Real)
-    x = transform(b, y)
-    return x, -x
-end
-function with_logabsdet_jacobian(b::Elementwise{typeof(log)}, y)
-    x = transform(b, y)
-    return x, -sum(x)
-end
