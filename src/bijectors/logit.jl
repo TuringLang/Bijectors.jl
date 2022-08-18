7956d05c48bf657585f6496178cc9e196ce0ea1a
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2022-08-18 16:17:26 +0100

    removed redundant comment

--- src/bijectors/logit.jl
+++ src/bijectors/logit.jl
@@ -11,8 +11,6 @@ Functors.@functor Logit
 # For equality of Logit with Float64 fields to one with Duals
 Base.:(==)(b1::Logit, b2::Logit) = b1.a == b2.a && b1.b == b2.b
 
-# TODO: Implement `forward` and batched versions.
-
 # Evaluation
 _logit(x, a, b) = LogExpFunctions.logit((x - a) / (b - a))
 transform(b::Logit, x) = _logit.(x, b.a, b.b)
