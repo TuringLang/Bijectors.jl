0bb86e21b6e97a6c34eed2ee6579e995abeef360
Hong Ge <hg344@cam.ac.uk>
2023-01-06 22:33:46 +0000

    Setup docs building.

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+using Documenter
+using Bijectors
+
+# Doctest setup
+DocMeta.setdocmeta!(Bijectors, :DocTestSetup, :(using Bijectors); recursive=true)
+
+makedocs(
+    sitename = "Bijectors",
+    format = Documenter.HTML(),
+    modules = [Bijectors],
+    pages = ["Home" => "index.md", "Distributions.jl integration" => "distributions.md", "Examples" => "examples.md"],
+    strict=false,
+    checkdocs=:exports,
+)
+
+deploydocs(repo = "github.com/TuringLang/Bijectors.jl.git", push_preview=true)
