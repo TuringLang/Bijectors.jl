e606dec12407491766dc90775e5e08bec9a96fcb
Seth Axen <seth@sethaxen.com>
2023-01-30 20:42:14 +0100

    Add ordered test

--- test/bijectors/ordered.jl
+++ test/bijectors/ordered.jl
@@ -1,4 +1,5 @@
-import Bijectors: OrderedBijector
+import Bijectors: OrderedBijector, ordered
+using LinearAlgebra
 
 @testset "OrderedBijector" begin
     b = OrderedBijector()
@@ -19,4 +20,23 @@ import Bijectors: OrderedBijector
     ys = b(xs)
     @test sort(ys[:, 1]) == ys[:, 1]
     @test sort(ys[:, 2]) == ys[:, 2]
+end
+
+@testset "ordered" begin
+    d = MvNormal(1:5, Diagonal(6:10))
+    d_ordered = ordered(d)
+    @test d_ordered isa Bijectors.TransformedDistribution
+    @test d_ordered.dist === d
+    @test d_ordered.transform isa OrderedBijector
+    y = randn(5)
+    x = inv(bijector(d_ordered))(y)
+    @test issorted(x)
+
+    d = Product(fill(Normal(), 5))
+    # currently errors because `bijector(Product(fill(Normal(), 5)))` is not an `Identity`
+    @test_broken ordered(d) isa Bijectors.TransformedDistribution
+
+    # non-Identity bijector is not supported
+    d = Dirichlet(ones(5))
+    @test_throws ArgumentError ordered(d)
 end
