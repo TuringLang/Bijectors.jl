5fd0a652b107fa4e12d4e9fdfc242e949d06d6d1
harisorgn <organtzh@gmail.com>
2023-04-12 18:13:11 +0100

    better utility function call

--- src/utils.jl
+++ src/utils.jl
@@ -14,7 +14,7 @@ upper_triangular(A::AbstractMatrix) = convert(typeof(A), UpperTriangular(A))
 pd_from_lower(X) = LowerTriangular(X) * LowerTriangular(X)'
 pd_from_upper(X) = UpperTriangular(X)' * UpperTriangular(X)
 
-cholesky_factor(X::AbstractMatrix) = cholesky(X).UL
+cholesky_factor(X::AbstractMatrix) = cholesky_factor(cholesky(X))
 cholesky_factor(X::Cholesky) = X.UL
 cholesky_factor(X::UpperTriangular) = X
 cholesky_factor(X::LowerTriangular) = X
