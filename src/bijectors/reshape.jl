1e57a744aa87b1d85bb58431d908e4d5f0f9b56b
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-05-15 12:02:35 +0100

    added docstring to Reshape

--- src/bijectors/reshape.jl
+++ src/bijectors/reshape.jl
@@ -1,3 +1,22 @@
+"""
+    Reshape(in_shape, out_shape)
+
+A [`Bijector`](@ref) that reshapes the input to the output shape.
+
+# Example
+
+```jldoctest
+julia> using Bijectors: Reshape
+
+julia> b = Reshape((2, 3), (3, 2))
+Reshape{Tuple{Int64, Int64}, Tuple{Int64, Int64}}((2, 3), (3, 2))
+
+julia> Array(transform(b, reshape(1:6, 2, 3)))
+3×2 Matrix{Int64}:
+ 1  4
+ 2  5
+ 3  6
+"""
 struct Reshape{S1,S2} <: Bijector
     in_shape::S1
     out_shape::S2
