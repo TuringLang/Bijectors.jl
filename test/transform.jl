37cfd90781f5a8c9b26800c7911d77b181a8669d
harisorgn <organtzh@gmail.com>
2023-05-24 19:07:10 +0300

    fix `Union` in `@inferred` after splitting structs

--- test/transform.jl
+++ test/transform.jl
@@ -39,17 +39,13 @@ function single_sample_tests(dist)
     # Check that invlink is inverse of link.
     x = rand(dist)
 
-    # LKJCholesky and LKJ use the same VecCorrBijector.
-    # The return type of Inverse{VecCorrBijector} depends on the distribution.
     if dist isa LKJCholesky
         x_inv = @inferred Union{Cholesky{Float64, Matrix{Float64}}, Matrix{Float64}} invlink(dist, link(dist, copy(x)))
         @test x_inv.UL ≈ x.UL atol=1e-9
-    elseif dist isa LKJ
-        x_inv = @inferred Union{Cholesky{Float64, Matrix{Float64}}, Matrix{Float64}} invlink(dist, link(dist, copy(x)))
-        @test x_inv ≈ x atol=1e-9
     else
         @test @inferred(invlink(dist, link(dist, copy(x)))) ≈ x atol=1e-9
     end
+
     # Check that link is inverse of invlink. Hopefully this just holds given the above...
     y = @inferred(link(dist, x))
     if dist isa Dirichlet
