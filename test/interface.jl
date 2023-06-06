7e2927afaa47cd94b84a659496d6217e2534fcf6
Seth Axen <seth@sethaxen.com>
2023-06-06 22:14:53 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/interface.jl
+++ test/interface.jl
@@ -168,10 +168,11 @@ end
                 end
                 y = b(x)
                 @test b(param(x)) isa TrackedArray
-                @test log(abs(det(ForwardDiff.jacobian(b, x)[1:end,1:end-1]))) ≈
+                @test log(abs(det(ForwardDiff.jacobian(b, x)[1:end, 1:(end - 1)]))) ≈
                     logabsdetjac(b, x)
-                @test log(abs(det(ForwardDiff.jacobian(inverse(b), y)[1:end-1,1:end]))) ≈
-                    logabsdetjac(inverse(b), y)
+                @test log(
+                    abs(det(ForwardDiff.jacobian(inverse(b), y)[1:(end - 1), 1:end]))
+                ) ≈ logabsdetjac(inverse(b), y)
             else
                 b = bijector(dist)
                 x = rand(dist)
