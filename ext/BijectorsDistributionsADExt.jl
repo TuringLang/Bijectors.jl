35b928f9504364ae41c3e2d85fae3bf0841739e3
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-06-06 16:12:15 +0100

    moved DistributionsAD to ext

--- ext/BijectorsDistributionsADExt.jl
+++ ext/BijectorsDistributionsADExt.jl
@@ -1,7 +1,15 @@
-using .DistributionsAD: TuringDirichlet, TuringWishart, TuringInverseWishart,
-                        FillVectorOfUnivariate, FillMatrixOfUnivariate,
-                        MatrixOfUnivariate, FillVectorOfMultivariate, VectorOfMultivariate,
-                        TuringScalMvNormal, TuringDiagMvNormal, TuringDenseMvNormal
+using .DistributionsAD:
+    TuringDirichlet,
+    TuringWishart,
+    TuringInverseWishart,
+    FillVectorOfUnivariate,
+    FillMatrixOfUnivariate,
+    MatrixOfUnivariate,
+    FillVectorOfMultivariate,
+    VectorOfMultivariate,
+    TuringScalMvNormal,
+    TuringDiagMvNormal,
+    TuringDenseMvNormal
 using Distributions: AbstractMvLogNormal
 
 # Bijectors
@@ -20,26 +28,26 @@ bijector(d::MatrixOfUnivariate{Continuous}) = TruncatedBijector(_minmax(d.dists)
 bijector(d::VectorOfMultivariate{Discrete}) = identity
 for T in (:VectorOfMultivariate, :FillVectorOfMultivariate)
     @eval begin
-        bijector(d::$T{Continuous, <:MvNormal}) = identity
-        bijector(d::$T{Continuous, <:TuringScalMvNormal}) = identity
-        bijector(d::$T{Continuous, <:TuringDiagMvNormal}) = identity
-        bijector(d::$T{Continuous, <:TuringDenseMvNormal}) = identity
-        bijector(d::$T{Continuous, <:MvNormalCanon}) = identity
-        bijector(d::$T{Continuous, <:AbstractMvLogNormal}) = Log()
-        bijector(d::$T{Continuous, <:SimplexDistribution}) = SimplexBijector()
-        bijector(d::$T{Continuous, <:TuringDirichlet}) = SimplexBijector()
+        bijector(d::$T{Continuous,<:MvNormal}) = identity
+        bijector(d::$T{Continuous,<:TuringScalMvNormal}) = identity
+        bijector(d::$T{Continuous,<:TuringDiagMvNormal}) = identity
+        bijector(d::$T{Continuous,<:TuringDenseMvNormal}) = identity
+        bijector(d::$T{Continuous,<:MvNormalCanon}) = identity
+        bijector(d::$T{Continuous,<:AbstractMvLogNormal}) = Log()
+        bijector(d::$T{Continuous,<:SimplexDistribution}) = SimplexBijector()
+        bijector(d::$T{Continuous,<:TuringDirichlet}) = SimplexBijector()
     end
 end
 bijector(d::FillVectorOfMultivariate{Continuous}) = bijector(d.dists.value)
 
-isdirichlet(::VectorOfMultivariate{Continuous, <:Dirichlet}) = true
-isdirichlet(::VectorOfMultivariate{Continuous, <:TuringDirichlet}) = true
+isdirichlet(::VectorOfMultivariate{Continuous,<:Dirichlet}) = true
+isdirichlet(::VectorOfMultivariate{Continuous,<:TuringDirichlet}) = true
 isdirichlet(::TuringDirichlet) = true
 
 function link(
     d::TuringDirichlet,
     x::AbstractVecOrMat{<:Real},
-    ::Val{proj}=Val(true),
+    ::Val{proj} = Val(true),
 ) where {proj}
     return SimplexBijector{proj}()(x)
 end
@@ -47,7 +55,7 @@ end
 function link_jacobian(
     d::TuringDirichlet,
     x::AbstractVector{<:Real},
-    ::Val{proj}=Val(true),
+    ::Val{proj} = Val(true),
 ) where {proj}
     return jacobian(SimplexBijector{proj}(), x)
 end
@@ -55,20 +63,20 @@ end
 function invlink(
     d::TuringDirichlet,
     y::AbstractVecOrMat{<:Real},
-    ::Val{proj}=Val(true),
+    ::Val{proj} = Val(true),
 ) where {proj}
     return inverse(SimplexBijector{proj}())(y)
 end
 function invlink_jacobian(
     d::TuringDirichlet,
     y::AbstractVector{<:Real},
-    ::Val{proj}=Val(true),
+    ::Val{proj} = Val(true),
 ) where {proj}
     return jacobian(inverse(SimplexBijector{proj}()), y)
 end
 
 ispd(::TuringWishart) = true
-ispd(::TuringInverseWishart) = true
+
 function getlogp(d::TuringWishart, Xcf, X)
     return ((d.df - (size(d, 1) + 1)) * logdet(Xcf) - tr(d.chol \ X)) / 2 + d.logc0
 end
