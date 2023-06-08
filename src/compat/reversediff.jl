1d8999f511b35b1ad908b14f681b094904ea669a
harisorgn <organtzh@gmail.com>
2023-06-08 15:01:37 +0300

    only use hacky `cholesky_factor` in versions before fix

--- src/compat/reversediff.jl
+++ src/compat/reversediff.jl
@@ -237,10 +237,13 @@ wrap_chainrules_output(x) = x
 wrap_chainrules_output(x::ChainRulesCore.AbstractZero) = nothing
 wrap_chainrules_output(x::Tuple) = map(wrap_chainrules_output, x)
 
-# HACK: To make it work for julia v1.6 .
-# This dispatch does not wrap X in Hermitian before calling cholesky. 
-# cholesky does not work with AbstractMatrix in julia v1.6,
-# and it would error with Hermitian{ReverseDiff.TrackedArray}.
-cholesky_factor(X::ReverseDiff.TrackedArray) = cholesky_factor(cholesky(X))
+if VERSION <= v"1.8.0-DEV.1526"
+    # HACK: This dispatch does not wrap X in Hermitian before calling cholesky. 
+    # cholesky does not work with AbstractMatrix in julia versions before the compared one,
+    # and it would error with Hermitian{ReverseDiff.TrackedArray}.
+    # See commit when the fix was introduced :
+    # https://github.com/JuliaLang/julia/commit/635449dabee81bba315ab066627a98f856141969
+    cholesky_factor(X::ReverseDiff.TrackedArray) = cholesky_factor(cholesky(X))
+end
 
 end
