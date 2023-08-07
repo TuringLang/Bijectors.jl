0bf8487edf081fc75e1e2cc2540a946aad312199
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-08-07 06:47:44 +0100

    added AD tests for PDVecBijector to runtests and commented out all other tests for the sake of reproducing ReverseDiff bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,22 +35,23 @@ const GROUP = get(ENV, "GROUP", "All")
 include("ad/utils.jl")
 include("bijectors/utils.jl")
 
-if GROUP == "All" || GROUP == "Interface"
-    include("interface.jl")
-    include("transform.jl")
-    include("norm_flows.jl")
-    include("bijectors/permute.jl")
-    include("bijectors/rational_quadratic_spline.jl")
-    include("bijectors/named_bijector.jl")
-    include("bijectors/leaky_relu.jl")
-    include("bijectors/coupling.jl")
-    include("bijectors/ordered.jl")
-    include("bijectors/pd.jl")
-    include("bijectors/reshape.jl")
-    include("bijectors/corr.jl")
-end
+# if GROUP == "All" || GROUP == "Interface"
+#     include("interface.jl")
+#     include("transform.jl")
+#     include("norm_flows.jl")
+#     include("bijectors/permute.jl")
+#     include("bijectors/rational_quadratic_spline.jl")
+#     include("bijectors/named_bijector.jl")
+#     include("bijectors/leaky_relu.jl")
+#     include("bijectors/coupling.jl")
+#     include("bijectors/ordered.jl")
+#     include("bijectors/pd.jl")
+#     include("bijectors/reshape.jl")
+#     include("bijectors/corr.jl")
+# end
 
 if GROUP == "All" || GROUP == "AD"
-    include("ad/chainrules.jl")
-    include("ad/flows.jl")
+    # include("ad/chainrules.jl")
+    # include("ad/flows.jl")
+    include("ad/pd.jl")
 end
