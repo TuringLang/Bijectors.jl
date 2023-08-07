1102266b047d8145a3552f83e0fee91342c58c63
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-08-07 10:05:12 +0100

    reversed chagne to test_ad

--- test/ad/utils.jl
+++ test/ad/utils.jl
@@ -1,36 +1,32 @@
 # Figure out which AD backend to test
 const AD = get(ENV, "AD", "All")
 
-function test_ad(f, x, broken=(); rtol=1e-6, atol=1e-6, use_forwarddiff_as_truth=false)
-    truth = if use_forwarddiff_as_truth
-        truth = ForwardDiff.gradient(f, x)
-    else
-        FiniteDifferences.grad(central_fdm(5, 1), f, x)[1]
-    end
+function test_ad(f, x, broken=(); rtol=1e-6, atol=1e-6)
+    finitediff = FiniteDifferences.grad(central_fdm(5, 1), f, x)[1]
 
-    if !use_forwarddiff_as_truth && (AD == "All" || AD == "ForwardDiff")
+    if AD == "All" || AD == "ForwardDiff"
         if :ForwardDiff in broken
-            @test_broken ForwardDiff.gradient(f, x) ≈ truth rtol = rtol atol = atol
+            @test_broken ForwardDiff.gradient(f, x) ≈ finitediff rtol = rtol atol = atol
         else
-            @test ForwardDiff.gradient(f, x) ≈ truth rtol = rtol atol = atol
+            @test ForwardDiff.gradient(f, x) ≈ finitediff rtol = rtol atol = atol
         end
     end
 
     if AD == "All" || AD == "Zygote"
         if :Zygote in broken
-            @test_broken Zygote.gradient(f, x)[1] ≈ truth rtol = rtol atol = atol
+            @test_broken Zygote.gradient(f, x)[1] ≈ finitediff rtol = rtol atol = atol
         else
             ∇zygote = Zygote.gradient(f, x)[1]
-            @test (all(truth .== 0) && ∇zygote === nothing) ||
-                isapprox(∇zygote, truth; rtol=rtol, atol=atol)
+            @test (all(finitediff .== 0) && ∇zygote === nothing) ||
+                isapprox(∇zygote, finitediff; rtol=rtol, atol=atol)
         end
     end
 
     if AD == "All" || AD == "ReverseDiff"
         if :ReverseDiff in broken
-            @test_broken ReverseDiff.gradient(f, x) ≈ truth rtol = rtol atol = atol
+            @test_broken ReverseDiff.gradient(f, x) ≈ finitediff rtol = rtol atol = atol
         else
-            @test ReverseDiff.gradient(f, x) ≈ truth rtol = rtol atol = atol
+            @test ReverseDiff.gradient(f, x) ≈ finitediff rtol = rtol atol = atol
         end
     end
 
