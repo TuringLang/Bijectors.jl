181e7d518895bb17c415730a33e798b58f6e03e7
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-08-07 11:50:03 +0100

    added tests for CorrVecBijector

---
+++ test/ad/corr.jl
@@ -0,0 +1,17 @@
+@testset "AD for CorrBijector" begin
+    d = 4
+    dist = LKJ(d, 2.0)
+    b = bijector(dist)
+    binv = inverse(b)
+
+    x = rand(dist)
+    y = b(x)
+
+    test_ad(y) do x
+        sum(transform(b, binv(x)))
+    end
+
+    test_ad(y) do y
+        sum(transform(binv, y))
+    end
+end
