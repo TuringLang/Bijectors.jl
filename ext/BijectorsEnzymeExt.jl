1239040f0533dd6277c0fffd007fec1e34d61834
Markus Hauru <mhauru@turing.ac.uk>
2024-06-28 09:28:43 +0100

    Fixes to Enzyme extension

--- ext/BijectorsEnzymeExt.jl
+++ ext/BijectorsEnzymeExt.jl
@@ -4,11 +4,11 @@ if isdefined(Base, :get_extension)
     using Enzyme: @import_frule, @import_rrule
     using Bijectors: find_alpha
 else
-    using ..Tapir: @import_frule, @import_rrule
+    using ..Enzyme: @import_frule, @import_rrule
     using ..Bijectors: find_alpha
 end
 
-@import_rrule typeof(find_alpha) Float64 Float64 Float64
-@import_frule typeof(find_alpha) Float64 Float64 Float64
+@import_rrule typeof(find_alpha) Real Real Real
+@import_frule typeof(find_alpha) Real Real Real
 
 end
