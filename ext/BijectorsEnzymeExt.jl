29e72fde0c807732aa62fa3b1712904f3b325bde
Penelope Yong <penelopeysm@gmail.com>
2024-10-24 00:36:33 +0100

    Disable precompilation of BijectorsEnzymeExt

--- ext/BijectorsEnzymeExt.jl
+++ ext/BijectorsEnzymeExt.jl
@@ -8,7 +8,23 @@ else
     using ..Bijectors: find_alpha
 end
 
-@import_rrule typeof(find_alpha) Real Real Real
-@import_frule typeof(find_alpha) Real Real Real
+# Julia 1.11.1 caused a change in the ordering of precompilation for extensions.
+# https://github.com/TuringLang/Bijectors.jl/issues/332
+# See https://github.com/JuliaLang/julia/issues/56204
+@static if v"1.11.1" <= VERSION < v"1.12"
+    function __init__()
+        if !Base.generating_output()
+            eval(
+                quote
+                    @import_rrule typeof(find_alpha) Real Real Real
+                    @import_frule typeof(find_alpha) Real Real Real
+                end,
+            )
+        end
+    end
+else
+    @import_rrule typeof(find_alpha) Real Real Real
+    @import_frule typeof(find_alpha) Real Real Real
+end
 
 end
