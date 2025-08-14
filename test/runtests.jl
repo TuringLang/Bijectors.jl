eb4bdfb77ba24825c9e9c3e1b3d0e68561caeaaa
Penelope Yong <penelopeysm@gmail.com>
2025-08-14 23:28:25 +0100

    Re-enable Enzyme tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,7 @@ using Combinatorics
 using DifferentiationInterface
 using DistributionsAD
 using Documenter: Documenter
-using Enzyme
-using EnzymeTestUtils
+using Enzyme: set_runtime_activity, Forward, Reverse
 using FiniteDifferences
 using ForwardDiff
 using Functors
@@ -42,27 +41,30 @@ TEST_ADTYPES = [
 ]
 if !IS_PRERELEASE
     push!(TEST_ADTYPES, ("Mooncake", AutoMooncake()))
+    push!(TEST_ADTYPES, ("EnzymeForward", AutoEnzyme(; mode=set_runtime_activity(Forward))))
+    push!(TEST_ADTYPES, ("EnzymeReverse", AutoEnzyme(; mode=set_runtime_activity(Reverse))))
 end
 
 # Always include this since it can be useful for other tests.
 include("bijectors/utils.jl")
 
 if GROUP == "All" || GROUP == "Interface"
-    # include("interface.jl")
-    # include("transform.jl")
-    # include("norm_flows.jl")
-    # include("bijectors/permute.jl")
-    # include("bijectors/rational_quadratic_spline.jl")
-    # include("bijectors/named_bijector.jl")
-    # include("bijectors/leaky_relu.jl")
-    # include("bijectors/coupling.jl")
-    # include("bijectors/ordered.jl")
-    # include("bijectors/pd.jl")
-    # include("bijectors/reshape.jl")
-    # include("bijectors/corr.jl")
-    # include("bijectors/product_bijector.jl")
-    # include("distributionsad.jl")
+    include("interface.jl")
+    include("transform.jl")
+    include("norm_flows.jl")
+    include("bijectors/permute.jl")
+    include("bijectors/rational_quadratic_spline.jl")
+    include("bijectors/named_bijector.jl")
+    include("bijectors/leaky_relu.jl")
+    include("bijectors/coupling.jl")
+    include("bijectors/ordered.jl")
+    include("bijectors/pd.jl")
+    include("bijectors/reshape.jl")
+    include("bijectors/corr.jl")
+    include("bijectors/product_bijector.jl")
+    include("distributionsad.jl")
 end
+
 if GROUP == "All" || GROUP == "AD"
     const REF_BACKEND = AutoFiniteDifferences(; fdm=central_fdm(5, 1))
     function test_ad(f, backend, x; rtol=1e-6, atol=1e-6)
@@ -70,23 +72,24 @@ if GROUP == "All" || GROUP == "AD"
         gradient = DifferentiationInterface.gradient(f, backend, x)
         @test isapprox(gradient, ref_gradient; rtol=rtol, atol=atol)
     end
-    # include("ad/chainrules.jl")
-    # include("ad/flows.jl")
-    # include("ad/pd.jl")
-    # include("ad/corr.jl")
-    # include("ad/stacked.jl")
-    include("ad/enzyme_rules.jl")
+    include("ad/chainrules.jl")
+    include("ad/flows.jl")
+    include("ad/pd.jl")
+    include("ad/corr.jl")
+    include("ad/stacked.jl")
+    include("ad/enzyme.jl")
 end
+
 if GROUP == "All" || GROUP == "Doctests"
-    # @testset "doctests" begin
-    #     Documenter.DocMeta.setdocmeta!(
-    #         Bijectors, :DocTestSetup, :(using Bijectors); recursive=true
-    #     )
-    #     doctestfilters = [
-    #         # Ignore the source of a warning in the doctest output, since this is dependent
-    #         # on host. This is a line that starts with "└ @ " and ends with the line number.
-    #         r"└ @ .+:[0-9]+",
-    #     ]
-    #     Documenter.doctest(Bijectors; manual=false, doctestfilters=doctestfilters)
-    # end
+    @testset "doctests" begin
+        Documenter.DocMeta.setdocmeta!(
+            Bijectors, :DocTestSetup, :(using Bijectors); recursive=true
+        )
+        doctestfilters = [
+            # Ignore the source of a warning in the doctest output, since this is dependent
+            # on host. This is a line that starts with "└ @ " and ends with the line number.
+            r"└ @ .+:[0-9]+",
+        ]
+        Documenter.doctest(Bijectors; manual=false, doctestfilters=doctestfilters)
+    end
 end
