7972e846d58d4043a32120cdb3ad919cd1fe8c0c
Penelope Yong <penelopeysm@gmail.com>
2025-10-17 13:24:04 +0100

    Another typo

--- test/ad/corr.jl
+++ test/ad/corr.jl
@@ -1,5 +1,3 @@
-using Enzyme: ForwardMode
-
 @testset "VecCorrBijector: $backend_name" for (backend_name, adtype) in TEST_ADTYPES
     # Enzyme is tested separately as these tests are flaky
     # TODO(penelopeysm): Fix upstream and re-enable.
