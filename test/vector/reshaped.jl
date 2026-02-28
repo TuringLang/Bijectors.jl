c619a7faf912692e20bc5f742b4af18ca79a9bd2
Penelope Yong <penelopeysm@gmail.com>
2026-02-28 13:54:47 +0000

    Skip failing Enzyme test

--- test/vector/reshaped.jl
+++ test/vector/reshaped.jl
@@ -16,7 +16,6 @@ reshaped = [
     vec(Normal()),
     reshape(Normal(), (1, 1, 1, 1, 1)),
     vec(Beta(2, 2)),
-    reshape(Beta(2, 2), (1, 1, 1, 1, 1)),
     vec(Poisson(3)),
     reshape(Poisson(3), (1, 1, 1, 1, 1)),
     reshape(MvNormal(zeros(2), I), (2, 1, 1)),
@@ -28,9 +27,27 @@ reshaped = [
     reshape(Wishart(7, Matrix{Float64}(I, 4, 4)), 1, 1, 4, 1, 4),
 ]
 
+# Fails on 1.10: https://github.com/EnzymeAD/Enzyme.jl/issues/2987
+adtypes_no_enz_rvs = [
+    DI.AutoReverseDiff(),
+    DI.AutoReverseDiff(; compile=true),
+    DI.AutoMooncake(),
+    DI.AutoMooncakeForward(),
+    DI.AutoEnzyme(; mode=EC.Forward, function_annotation=EC.Const),
+]
+reshaped_no_enzyme = [reshape(Beta(2, 2), (1, 1, 1, 1, 1))]
+
 @testset "Reshaped distributions" begin
     for d in reshaped
         VectorBijectors.test_all(d; expected_zero_allocs=())
+    end
+
+    for d in reshaped_no_enzyme
+        @static if VERSION >= v"1.11-"
+            VectorBijectors.test_all(d; expected_zero_allocs=())
+        else
+            VectorBijectors.test_all(d; adtypes=adtypes_no_enz_rvs, expected_zero_allocs=())
+        end
     end
 end
 
