fe66b200819263417d36538ea8cf56e280e01b8a
Penelope Yong <penelopeysm@gmail.com>
2026-03-02 21:27:18 +0000

    Add size to Vec

--- src/vector/matrix/matrix.jl
+++ src/vector/matrix/matrix.jl
@@ -2,8 +2,12 @@
 
 # Somehow, ChangesOfVariables doesn't have a logjac implemented for `vec`, so we need to
 # wrap it.
-struct Vec end
+struct Vec{N}
+    size::NTuple{N,Int}
+end
 (::Vec)(x::AbstractArray) = vec(x)
+inverse(v::Vec) = Reshape(v.size)
+
 function with_logabsdet_jacobian(::Vec, x::AbstractArray{T,N}) where {T<:Number,N}
     return vec(x), zero(T)
 end
@@ -11,10 +15,11 @@ function with_logabsdet_jacobian(::Vec, x::AbstractArray)
     return vec(x), 0.0
 end
 
-struct Reshape{N,T<:NTuple{N,Int}}
-    size::T
+struct Reshape{N}
+    size::NTuple{N,Int}
 end
 (r::Reshape)(x::AbstractArray) = reshape(x, r.size)
+inverse(r::Reshape) = Vec(r.size)
 function with_logabsdet_jacobian(r::Reshape, x::AbstractArray{T,N}) where {T<:Number,N}
     return reshape(x, r.size), zero(T)
 end
@@ -22,7 +27,7 @@ function with_logabsdet_jacobian(r::Reshape, x::AbstractArray)
     return reshape(x, r.size), 0.0
 end
 
-to_vec(::D.MatrixDistribution) = Vec()
+to_vec(d::D.MatrixDistribution) = Vec(size(d))
 from_vec(d::D.MatrixDistribution) = Reshape(size(d))
 vec_length(d::D.MatrixDistribution) = prod(size(d))
 function optic_vec(d::D.MatrixDistribution)
