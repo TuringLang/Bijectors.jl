3be6995b357581fea97e1f40f6852e21fd5fd36e
Penelope Yong <penelopeysm@gmail.com>
2026-03-03 12:09:38 +0000

    Update src/vector/matrix/normal.jl

--- src/vector/matrix/normal.jl
+++ src/vector/matrix/normal.jl
@@ -3,7 +3,7 @@
 
 const UnconsMatrixDist = Union{D.MatrixNormal,D.MatrixTDist}
 
-to_linked_vec(::UnconsMatrixDist) = Vec()
+to_linked_vec(d::UnconsMatrixDist) = Vec(size(d))
 from_linked_vec(d::UnconsMatrixDist) = Reshape(size(d))
 linked_vec_length(d::UnconsMatrixDist) = prod(size(d))
 function linked_optic_vec(d::UnconsMatrixDist)
