f80ae62718230b02617b46c6ac51b9071ac52719
Penelope Yong <penelopeysm@gmail.com>
2026-04-24 17:58:47 +0100

    Make a better test

--- test/vector/transformed.jl
+++ test/vector/transformed.jl
@@ -14,9 +14,9 @@ transformed_dists = [
     # Univariate
     transformed(Normal(), exp),
     transformed(Beta(2, 3), Bijectors.Logit(0.0, 1.0)),
-    transformed(Gamma(2, 1), Bijectors.Log{0}()),
+    transformed(Gamma(2, 1), elementwise(log)),
     # Multivariate
-    transformed(product_distribution(fill(Normal(), 4)), elementwise(exp)),
+    transformed(product_distribution(fill(Cauchy(), 4)), elementwise(exp)),
     transformed(MvNormal(zeros(3), I), Bijectors.Scale(2.0)),
     transformed(Dirichlet([1.0, 2.0, 3.0])),
     transformed(MvLogNormal(zeros(2), I), elementwise(log)),
